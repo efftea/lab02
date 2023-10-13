343ae49b7437444a7ecabfe9a3017ac24c4a9fec
efftea <daanikwork@mail.ru>
2023-10-13 23:54:44 +0300

    Измена code style cpp файлов

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 
-int main(){
-	string name;
-	count << "Enter your name: ";
-	cin >> name;
-	count << "Hello world from @" << name << endl;
-	return 0;
+int
+main()
+{
+  string name;
+  count << "Enter your name: ";
+  cin >> name;
+  count << "Hello world from @" << name << endl;
+  return 0;
 }
